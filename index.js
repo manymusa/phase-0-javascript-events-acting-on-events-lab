d2cc4543f00ce0940d35cdc5930335d51cccb8cf
adan <adanalin113@gmail.com>
2021-08-16 16:29:30 -0400

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft(){
+    const leftNumber = dodger.style.left.replace('px','');
+    const left = parseInt(leftNumber, 10); 
+     if(left>0){
+         dodger.style.left = `${left-1}px`;
+     }
+}
+
+function moveDodgerRight(){
+    const rightNumber = dodger.style.left.replace('px','');
+    const right = parseInt(rightNumber, 10);
+    if(right<360){
+        dodger.style.left = `${right+1}px`
+    }
+} 
+
+
+document.addEventListener('keydown', function(event){
+    if(event.key === 'ArrowLeft'){
+        moveDodgerLeft();
+    }
+    if(event.key === 'ArrowRight'){
+        moveDodgerRight();
+    }
+});
+
+
